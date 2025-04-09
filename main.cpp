6406f825d7b1699a747bf4b42eb2c6f1d4274e8e
Matveev Stepan Andreevich <mset321@gmail.com>
2025-04-09 17:41:23 +0300

    feat: add graph class

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <cstddef>
+#include <exception>
+#include <iostream>
+
+#include "src/graph.hpp"
+
+namespace {
+    graph::Graph readGraph(std::istream& is) {
+        using namespace graph;
+
+        std::size_t V = 0;
+        is >> V;
+
+        std::size_t E = 0;
+        is >> E;
+
+        Graph g(V);
+
+        while (E--) {
+            Vertex_t v, w;
+            is >> v >> w;
+            g.addEdge(v, w);
+        }
+
+        return g;
+    }
+} // namespace
+
+
+int main() try { 
+    auto g = readGraph(std::cin);
+} catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+} catch (...) {
+    std::cerr << "Unknown exception\n";
+} 
---
+++ src/graph.hpp
@@ -0,0 +1,35 @@
+#ifndef SRC_GRAPH_HPP
+#define SRC_GRAPH_HPP
+
+#include <vector>
+#include <cstddef>
+
+namespace graph {
+     
+using Vertex_t = std::size_t;
+
+// без гарантии исключений
+class Graph final {
+    std::vector<std::vector<Vertex_t>> adjs_;
+    std::size_t E_;
+
+public:
+    Graph(std::size_t V) : adjs_(V) {}
+
+public:
+    void addEdge(Vertex_t v, Vertex_t w) {
+        adjs_[v].push_back(w);
+        adjs_[w].push_back(v);
+    }
+
+    decltype(auto) adj(Vertex_t v) const noexcept {
+        return adjs_[v];
+    }
+
+    auto E() const noexcept { return E_; }
+    auto V() const noexcept { return adjs_.size(); }
+};
+
+} // namespace graph
+
+#endif // SRC_GRAPH_HPP
